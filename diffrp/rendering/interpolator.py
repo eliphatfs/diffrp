a94ad543e4113092fcf415c9259fca304e3c958d
eliphatfs <shiruoxi61@gmail.com>
2024-07-30 12:12:21 +0800

    Ensure contiguous layout for interpolator input.

--- diffrp/rendering/interpolator.py
+++ diffrp/rendering/interpolator.py
@@ -5,9 +5,9 @@ import nvdiffrast.torch as dr
 class Interpolator:
     
     def __init__(self, vi_data: torch.Tensor, tris: torch.IntTensor):
-        self.vi_data = vi_data  # (u, v, z|depth, tridx)
-        self.tris = tris
+        self.vi_data = vi_data.contiguous()  # (u, v, z|depth, tridx)
+        self.tris = tris.contiguous()
 
     def interpolate(self, vertex_buffer: torch.Tensor):
-        attr, da = dr.interpolate(vertex_buffer, self.vi_data, self.tris)
+        attr, da = dr.interpolate(vertex_buffer.contiguous(), self.vi_data, self.tris)
         return attr
