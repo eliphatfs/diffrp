c8a837c35a42f40a9cd9c1605c0b4c9e00d7f386
eliphatfs <shiruoxi61@gmail.com>
2024-08-03 11:55:04 +0800

    Minor improvements in tone mapping impl.

--- diffrp/utils/tone_mapping.py
+++ diffrp/utils/tone_mapping.py
@@ -1,18 +1,22 @@
 import torch
-import torch.nn.functional as F
+from .cache import key_cached
+from .shader_ops import sample3d
 from ..resources import get_resource_path
 from .colors import linear_to_alexa_logc_ei1000, linear_to_srgb
 
 
+class AgxLutLoader:
+    @key_cached
+    def load(self, variant: str) -> torch.Tensor:
+        return torch.load(get_resource_path("luts/agx-%s.pt" % variant))
+
+
+agx_lut_loader = AgxLutLoader()
+
+
 def agx_base_contrast(rgb: torch.Tensor):
-    lut: torch.Tensor = torch.load(get_resource_path("luts/agx-base-contrast.pt"))
+    lut = agx_lut_loader.load("base-contrast")
     lut = lut.to(rgb)  # z y x 3
     logc = linear_to_alexa_logc_ei1000(rgb)  # h w 3
     # sampled: 1 c 1 h w
-    return linear_to_srgb(F.grid_sample(
-        lut[None].permute(0, 4, 1, 2, 3),
-        logc[None, None] * 2 - 1,
-        mode='bilinear',
-        padding_mode='border',
-        align_corners=False
-    ).permute(0, 2, 3, 4, 1).reshape_as(rgb))
+    return linear_to_srgb(sample3d(torch.fliplr(lut), logc))
